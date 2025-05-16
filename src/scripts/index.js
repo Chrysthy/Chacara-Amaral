18ce5706f572135e146da0d4afb28334cea8ed44
Chrystine Martins <chrystine.martins@outlook.com>
2025-05-15 21:24:59 -0300

    update content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,19 @@
 window.botpress.init({
     "botId": "d936cf5b-e36d-46d9-b7bd-26e9adb0fd9b",
     "configuration": {
+        "composerPlaceholder": "Digite \"oi\" para começar!",
+        "botName": "Chácara Amaral",
         "website": {},
         "email": {},
         "phone": {},
         "termsOfService": {},
         "privacyPolicy": {},
-        "color": "#008000",
-        "variant": "soft",
+        "color": "#46a758",
+        "variant": "solid",
         "themeMode": "light",
-        "fontFamily": "inter",
-        "radius": 1,
-        "botConversationLanguage": "pt",
-        // "showPoweredBy": false,
-        "botName": "Assistente Chácara Amaral",
-        "composerPlaceholder": "Digite oi para começar!"
+        "fontFamily": "rubik",
+        "radius": 0.5,
+        "allowFileUpload": false
     },
     "clientId": "abaf3b01-c4e1-4d95-850e-1c12844a748d",
     "selector": "#webchat"
@@ -23,3 +22,4 @@ window.botpress.init({
 // window.botpress.on("webchat:ready", () => {
 //     window.botpress.open();
 // });
+
